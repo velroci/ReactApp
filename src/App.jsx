e724f64bafb8aa536609460bc1226dd3e52d75c5
velroci <rossielpz@gmail.com>
2024-01-20 18:59:53 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+       
+      </div>
+     
+      
+      
+    </>
+  )
+}
+
+export default App
