8f73912973fb0f45feea0dab2fef857f4b6b31ac
velroci <rossielpz@gmail.com>
2024-01-24 20:44:26 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
+import { useState } from 'react'
 
 function App() {
- 
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  return (
-    <>
+  function handleClick() {
+    {featPupId && (
       <div>
-       
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
       </div>
-     
-      
-      
-    </>
-  )
+    )}
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App
